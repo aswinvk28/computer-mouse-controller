dd270b95dfac4f7184e277ed6fda61041e120f6d
Aswin Vijayakumar <aswinvk28@gmail.com>
2020-06-06 01:13:49 +0100

    added bin directory

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aswin/Documents/Courses/Udacity/Intel-Edge-Phase2/Projects/Computer-Pointer-Controller/starter/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
